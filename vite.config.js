bb045a92129e3aed14181276d63a32e16b40e94d
Say <say.xpe1578@gmail.com>
2025-10-18 21:13:03 +0300

    Fix path for shadcn & project // Moved index.css in styles folder

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,22 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-
   base: './',
-
   server: {
     port: 5173,  
   },
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@main': path.resolve(__dirname, 'src/main'),
+      '@renderer': path.resolve(__dirname, 'src/renderer'),
+      '@components': path.resolve(__dirname, 'src/renderer/components'),
+      '@ui': path.resolve(__dirname, 'src/renderer/components/ui'),
+      '@hooks': path.resolve(__dirname, 'src/renderer/hooks'),
+      '@pages': path.resolve(__dirname, 'src/renderer/pages'),
+      '@styles': path.resolve(__dirname, 'src/renderer/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@prisma': path.resolve(__dirname, 'prisma'),
     },
   },
 });
