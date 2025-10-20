a315148af3fca9d9450f0797050263c29797ce2b
Say <say.xpe1578@gmail.com>
2025-10-20 21:01:16 +0300

    Update Layout

--- src/renderer/components/Layout.tsx
+++ src/renderer/components/Layout.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import Header from "@components/Header";
+import Sidebar from "@components/Sidebar";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1">
-        {children}
-      </main>
+    <div className="flex flex-col h-screen overflow-hidden">      
+      <Header />
+
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+
+        <main className="flex-1 overflow-auto p-4">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
